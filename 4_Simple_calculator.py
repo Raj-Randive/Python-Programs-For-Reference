adb59c35341aec07a199d1e46361e46f5e4731c7
Raj Randive <randiveraj96@gmail.com>
2022-07-25 14:52:55 +0530

    Passed in first trial.

---
+++ 4_Simple_calculator.py
@@ -0,0 +1,9 @@
+print("Enter the first num :", end=" ")
+first = int(input())
+print("Enter the Second num :", end=" ")
+second = int(input())
+
+print("sum is :", first + second)  # 1 space will come by default after the semicolon.
+print("Subtraction is :", first - second)
+print("Multiplication is :", first * second)
+print("Division is :",first / second)
